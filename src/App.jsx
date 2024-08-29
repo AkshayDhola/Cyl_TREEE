0573c48d8ca5044397a195b069908b31d6459ba3
AkshayDhola <akshaydhola3527@gamil.com>
2024-08-29 15:17:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import 'react';
+import Cyl from './Cyl';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { Bloom, EffectComposer, ToneMapping } from '@react-three/postprocessing'
+function App() {
+  
+  return (
+    <Canvas flat camera={{ fov:65}}>
+      <OrbitControls />
+      <ambientLight/>
+      <Cyl/>
+      <EffectComposer>
+      <Bloom
+      mipmapBlur
+    intensity={9.0} // The bloom intensity.
+    luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
+    luminanceSmoothing={0} // smoothness of the luminance threshold. Range is [0, 1]
+    />
+    </EffectComposer>
+    </Canvas>
+  )
+}
+
+export default App
---
+++ src/Cyl.jsx
@@ -0,0 +1,22 @@
+import React, { useRef } from 'react'
+import 'react'
+import { useTexture } from '@react-three/drei';
+import * as THREE from "three";
+import { useFrame } from '@react-three/fiber';
+function Cyl() {
+    let tex = useTexture("/vector.png");
+    let cyl = useRef(null);
+    useFrame((state,delta)=>{
+        cyl.current.rotation.y += delta;
+    })
+    return (
+        <group rotation={[0, 1.4, 0.5]}>
+      <mesh ref={cyl} >
+          <cylinderGeometry args={[2, 2, 2,60,60,true]} />
+          <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
+    </mesh>
+    </group>
+    )
+}
+
+export default Cyl
